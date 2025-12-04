83f3877704901bf82d40c402197ab2472ef374f6
claudia <claudia.nigro.cn@gmail.com>
2025-12-04 12:29:13 +0100

    Aggiunte altre funzioni

--- quiz.py
+++ quiz.py
@@ -1,10 +1,3 @@
-def valida_scelta(scelta:str) -> bool:
-    """Questa funzione controlla l'input dell'utente"""
-    scelta_tmp = scelta.upper()
-    if scelta_tmp == "A" or scelta_tmp == "B" or scelta_tmp == "C" or scelta_tmp =="D":
-        return True
-    else:
-        return False
 
 
 def mostra_domanda() -> None:
@@ -17,11 +10,43 @@ def mostra_domanda() -> None:
     D. Rocco Papaleo
     """)
 
+def valida_scelta(scelta:str) -> bool:
+    """Questa funzione controlla l'input dell'utente"""
+    scelta_tmp = scelta.upper()
+    if scelta_tmp == "A" or scelta_tmp == "B" or scelta_tmp == "C" or scelta_tmp =="D":
+        return True
+    else:
+        return False
+
 def raccogli_risposta() -> str:
     """Questa funzione prende input dell'utente"""
     return input("Inserisci la tua scelta: ")
 
+
+def genera_feedback(scelta:str) -> str:
+    """Questa funzione controlla se la risposta è giusta"""
+    if scelta.upper() == "A":
+        return "Hai indovinato!"
+    else:
+        return "Non hai indovinato!"
+    
+
+
+def mostra_feedback(messaggio : str) -> None:
+    simbolo : str = "*"*30
+    print(f"""
+{simbolo}
+{messaggio}
+{simbolo}"""  )
+
+
 risposta_da_validare : str = raccogli_risposta()
 risposta_validata : bool = valida_scelta(risposta_da_validare)
+feedback : str = ""
 
-print(risposta_validata)
+if risposta_validata == True:
+    feedback = genera_feedback(risposta_da_validare)
+else:
+    feedback = "Inserisce solo  la risposta tra le opzioni elencate: "
+
+mostra_feedback(feedback)    
